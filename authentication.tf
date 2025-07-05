fed706c240dc98c3cf1001c78ba402b66ca9b4c1
Ricardo Guardiola <ricardoguardiolahn@gmail.com>
2025-07-04 22:51:05 -0600

    update

--- authentication.tf
+++ authentication.tf
@@ -20,7 +20,7 @@ resource "azurerm_storage_account" "auth_storage" {
   account_replication_type = "LRS"
 
   # Configuración de seguridad
-  enable_https_traffic_only = true
+  # enable_https_traffic_only = true
   min_tls_version          = "TLS1_2"
   allow_nested_items_to_be_public = false
 
@@ -78,40 +78,41 @@ resource "azurerm_role_assignment" "storage_access" {
   principal_id         = azurerm_user_assigned_identity.main.principal_id
 }
 
-# Secretos específicos para autenticación
-resource "azurerm_key_vault_secret" "auth_encryption_key" {
-  name         = "auth-encryption-key"
-  value        = "your-encryption-key-for-passwords"
-  key_vault_id = azurerm_key_vault.main.id
+# # Secretos específicos para autenticación
+# resource "azurerm_key_vault_secret" "auth_encryption_key" {
+#   name         = "auth-encryption-key"
+#   value        = "your-encryption-key-for-passwords"
+#   key_vault_id = azurerm_key_vault.main.id
   
-  tags = var.tags
-  depends_on = [azurerm_key_vault.main]
-}
+  
+#   tags = var.tags
+#   depends_on = [azurerm_key_vault.main]
+# }
 
-resource "azurerm_key_vault_secret" "session_secret" {
-  name         = "session-secret"
-  value        = "your-session-secret-key"
-  key_vault_id = azurerm_key_vault.main.id
+# resource "azurerm_key_vault_secret" "session_secret" {
+#   name         = "session-secret"
+#   value        = "your-session-secret-key"
+#   key_vault_id = azurerm_key_vault.main.id
   
-  tags = var.tags
-  depends_on = [azurerm_key_vault.main]
-}
+#   tags = var.tags
+#   depends_on = [azurerm_key_vault.main]
+# }
 
-# OAuth/External provider secrets (Google, Facebook, etc.)
-resource "azurerm_key_vault_secret" "google_oauth_client_id" {
-  name         = "google-oauth-client-id"
-  value        = "your-google-oauth-client-id"
-  key_vault_id = azurerm_key_vault.main.id
+# # OAuth/External provider secrets (Google, Facebook, etc.)
+# resource "azurerm_key_vault_secret" "google_oauth_client_id" {
+#   name         = "google-oauth-client-id"
+#   value        = "your-google-oauth-client-id"
+#   key_vault_id = azurerm_key_vault.main.id
   
-  tags = var.tags
-  depends_on = [azurerm_key_vault.main]
-}
+#   tags = var.tags
+#   depends_on = [azurerm_key_vault.main]
+# }
 
-resource "azurerm_key_vault_secret" "google_oauth_client_secret" {
-  name         = "google-oauth-client-secret"
-  value        = "your-google-oauth-client-secret"
-  key_vault_id = azurerm_key_vault.main.id
+# resource "azurerm_key_vault_secret" "google_oauth_client_secret" {
+#   name         = "google-oauth-client-secret"
+#   value        = "your-google-oauth-client-secret"
+#   key_vault_id = azurerm_key_vault.main.id
   
-  tags = var.tags
-  depends_on = [azurerm_key_vault.main]
-}
+#   tags = var.tags
+#   depends_on = [azurerm_key_vault.main]
+# }
